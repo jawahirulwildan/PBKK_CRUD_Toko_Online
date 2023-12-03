3b58c7be26427d350e8dae0d482e24dc87ae8ac3
jawahirulwildan <jawahirulwildan@gmail.com>
2023-12-04 02:59:16 +0700

    initial commit

---
+++ application/models/model_kategori.php
@@ -0,0 +1,29 @@
+<?php
+
+class Model_kategori extends CI_Model
+{
+    public function data_elektronik()
+    {
+        return $this->db->get_where("tb_barang", array("kategori" => "Elektronik"));
+    }
+
+    public function data_pakaian_pria()
+    {
+        return $this->db->get_where("tb_barang", array("kategori" => "Pakaian Pria"));
+    }
+
+    public function data_pakaian_wanita()
+    {
+        return $this->db->get_where("tb_barang", array("kategori" => "Pakaian Wanita"));
+    }
+
+    public function data_pakaian_anak_anak()
+    {
+        return $this->db->get_where("tb_barang", array("kategori" => "Pakaian Anak-anak"));
+    }
+
+    public function data_peralatan_olahraga()
+    {
+        return $this->db->get_where("tb_barang", array("kategori" => "Peralatan Olahraga"));
+    }
+}
---
+++ application/views/pakaian_anak_anak.php
@@ -0,0 +1,16 @@
+<div class="container-fluid">
+    <div class="row text-center mt-4">
+        <?php foreach ($pakaian_anak_anak as $brg) : ?>
+            <div class="card ml-3 mb-3" style="width: 16rem;">
+                <img class="card-img-top" src="<?php echo base_url().'/uploads/'.$brg->gambar ?>" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title mb-1"><?php echo $brg->nama_brg ?></h5>
+                    <small><?php echo $brg->keterangan ?></small><br>
+                    <span class="badge badge-pill badge-success mb-3">Rp. <?php echo number_format($brg->harga, 0, ',', '.')  ?></span>
+                    <?php echo anchor('dashboard/tambah_ke_keranjang/' . $brg->id_brg, '<div class="btn btn-sm btn-primary">Tambah ke Keranjang</div>') ?>
+                    <?php echo anchor('dashboard/detail/' . $brg->id_brg, '<div class="btn btn-sm btn-success">Detail</div>') ?>
+                </div>
+            </div>
+        <?php endforeach?>
+    </div>
+</div>
